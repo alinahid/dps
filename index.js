6436e9562e3ea03662c47f5f1a4e6eda5b296094
alinahid <nahidali412@gmail.com>
2020-12-01 14:04:41 +0530

    first commit

--- index.js
+++ index.js
@@ -122,9 +122,9 @@ app.use(expressValidator({
   app.use('/admin/notice',noticeRouter);
   app.use('/', admission)
   app.use('/auth', require('./src/routes/auth'));
-
-app.listen(3000, ()=>{
-    console.log("server started on port 3000")
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log("server started ")
 })
  
 
